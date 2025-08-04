84826150a47e26416afd1d280e61dcc2b72d69ee
phucchinh2606 <chinhdo266@gmail.com>
2025-08-05 03:16:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import XO from "./Components/XO/XO";
+
+function App() {
+  return (
+    <div>
+      <XO />
+    </div>
+  );
+}
+
+export default App;
